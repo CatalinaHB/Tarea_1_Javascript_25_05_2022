2e49ab1e8b5aec3db23e3ec46e03802d67ccc3b3
catalinahb <cadehenba@gmail.com>
2022-05-25 16:54:01 -0400

    Tarea 1: Horoscopo

---
+++ main.js
@@ -0,0 +1,76 @@
+
+
+
+let continuar = confirm ("¿Te gustaría saber cual es tu animal en el hoscopo chino?")
+if (continuar == false)
+{
+    alert ("Bueno! Tu te lo pierdes!")
+}
+else
+año = parseInt (prompt ("Ingresa el año que naciste"))
+if (año==1936 || año==1948 || año==1960 || año==1972 || año==1984 || año==1996 || año==2008) 
+{
+alert("Tu animal es la RATA: Los nacidos bajo el signo de la Rata son persona sabias a las que les gusta rodearse de familiares y amigos, a quienes ayudan en sus quehaceres y problemas diarios. Buenas trabajadoras, ahorradores y buenos administradores de su economía")
+}
+else
+    if (año==1937 || año==1949|| año==1961|| año==1973|| año==1985|| año==1997|| año==2009)
+    {
+       alert("Tu animal es el  BUFALO: Los búfalos o bueyes son animales pacientes y tranquilos, que irradian mucho cariño y amor e infunden respeto. Grandes trabajadores, se sienten a gusto rodeados del orden y la limpieza y logran el éxito gracias a su esfuerzo. Para ellos lo más importante es la familia, aunque son celosos con su pareja. Aman el arte y la música. No les gusta discutir, aunque sí que le obedezcan.")
+    }
+    else
+       if(año==1938 || año==1950 || año==1962 || año==1974 || año==1986 || año==1998 || año==2010)
+       {
+           alert("Tu animal es el TIGRE: Las personas de este signo son muy pasionales y llenas de energía. No pasan desapercibidas, son aventureros, independientes, ingeniosos, impulsivos y les gusta la diversión. Son amigos para toda la vida, aunque les gusta ser el líder del grupo. En el mundo de las relaciones de pareja se muestran el más fuerte, sexy y seductor, aunque de comportamiento excesivo.")
+       }
+       else
+          if (año==1939 || año==1951 || año==1963 || año==1975 || año==1987 || año==1999 ||año==2011)
+          {
+              alert("Tu animal es el CONEJO: Los que nacen en el Año del Conejo reúnen extraordinarias cualidades humanas: son prudentes, inteligentes, afables, discretos, previsores, atentos y benevolentes. Por eso, el signo del conejo es ampliamente aceptado por la gente.De carácter moderado e indulgente, amante de la paz y la concordia, el conejo odia la guerra y la violencia. Le gusta la vida tranquila, la ternura y la armonía.")
+          }
+          else
+             if (año==1940 || año==1952 || año==1964 || año==1976 || año==1988 || año==2000 || año==2012)
+             {
+                 alert("Tu animal es el DRAGÓN: Símbolo del Emperador en China, el dragón es imaginativo, magnánimo, emprendedor, afortunado y poderoso. Está lleno de fuerza y vitalidad. Es un verdadero acumulador de energía y si le enojas, se enfurecerá de tal manera que perderá los estribos. Será difícil calmarle, pues no cree ni a nadie ni en nadie. Suelen dar buenos consejos y son afortunados tanto en el dinero como en el amor. Son muy sentimentales y se enamoran locamente.")
+             }
+             else
+               if (año==1941 || año==1953 || año==1965 || año==1977 || año==1989 || año==2001 || año==2013)
+               {
+                   alert("Tu animal es la SERPIENTE: Las personas Serpientes son astutas, saben lo que quieren y cómo conseguirlo. Su conversación es elegante y fluida. Son intensos y prudentes a la vez. Son celosas y posesivas con su familia pero leales. Les gusta mimar a sus amigos y esperan de ellos una actitud recíproca. Si se sienten desdeñados o fracasan en sus tentativas, pueden estar enfadados durante horas, aunque son buenas para mantener la calma y parecer tranquilas en situaciones difíciles.")
+               }
+               else
+                  if (año==1942 || año==1954 || año==1966 || año==1978 || año==1990 || año==2002 || año==2014)
+                  {
+                      alert("Tu animal es el CABALLO: Populares, optimistas, llenos de alegría, aventureros, elocuentes, impacientes, emprendedores, entusiastas con la vida, grandes amantes y amigos. Su gran pasión es el dinero y los viajes. Les gusta conocer gente nueva, distintas culturas, hablar idiomas y son el alma de las fiestas. Su atractivo físico y su belleza hace que les sea fácil encontrar el amor: trampa en la que caen sin remedio.")
+                  }
+                  else
+                     if (año==1943 || año==1955 || año==1967 || año==1979 || año==1991 || año==2003 || año==2015)
+                     {
+                         alert("Tu animal es la CABRA: La cabra es creativa, artística, positiva, elegante, femenina, afable, compasiva, llorona, sensible, soñadora, orgullosa y triste. No persiguen la riqueza material y prefieren vivir de sus ensoñaciones. Aman la familia, los niños, el hogar y estar en contacto con la naturaleza. Son muy trabajadoras, aunque no les gusta la presión ni de una fecha límite, ni de un jefe.")
+                     }
+                     else
+                        if (año==1944 ||  año==1956 || año==1968 ||  año==1980 || año==1992 ||  año==2004 ||  año==2016)
+                        {
+                            alert("Tu animal es el MONO: Animal más parecido al ser humano, es ingenioso, divertido, simpático, de mente rápida y despierta, comprador, persuasivo, sociable, capaces de resolver cualquier problema por difícil que sea con soltura. Sus relaciones amorosas y con los amigos suelen ser conflictivas, pues no soporta que no lo valoren. Gozan de buena salud, aunque sus excesos en las fiestas, alcohol y drogas podrían pasarle factura.")
+                        }
+                        else
+                           if (año==1945 || año==1957 || año==1969 || año==1981 || año==1993 || año==2005 || año==2017)
+                           {
+                               alert("Tu animal es el GALLO: Atractivo y seductor, con talento y apreciado. Son meticulosos, eficientes, ordenados, buenos conversadores, observadores, egoístas, usureros, pragmáticos y muy familiares. En el amor son románticos, obstinados y luchan por el control de su pareja. Suelen destacar como relaciones públicas o como artistas. No prestan demasiada atención a su salud.")
+                           }
+                           else
+                              if (año==1946 || año==1958 || año==1970 || año==1982 || año==1994 || año==2006 || año==2018)
+                              {
+                                  alert("Tu animal es el PERRO: El perro es un ser dedicado al trabajo, honesto, confiable, diligente, con un gran sentido de la justicia y la lealtad. Cautiva a todos con su personalidad excitante, su buen humor y su gran capacidad para escuchar los problemas de los demás y dar buenos consejos. En el amor son fieles y leales, aunque muy celoso cuando su pareja no está en casa. Los nervios son su mayor problema de salud.")
+                              }
+                              else
+                                 if (año==1947 || año==1959 || año==1971 || año==1983 || año==1995 || año==2007 || año==2019)
+                                 {
+                                     alert ("Tu animal es el CERDO: Es un animal sincero, honesto, confiado, educado, cariñoso, servicial, valiente, decidido, con gran fuerza de voluntad, sin grandes pretensiones ni vanidad, con los pies en la tierra. Son muy agradables y tienen gusto y modales impecables. En el amor son sensuales, apasionado y pacientes, pero hasta un punto. Son muy trabajadores, aunque la falta de atracción por el deporte y la apatía harán que su físico se resienta y engorde en exceso.")
+                                 }
+                                 else
+                                   if (año<1936 || año>2019)
+                                 {
+                                     alert("Sorry! Me pillaste! Sólo me sé el calendario chino entre 1936 y 2019")
+                                 }
+                                 
+                         
